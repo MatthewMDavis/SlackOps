68a85e1f1c70017b8bb7f596dbcf0e2c6f1e9d32
MatthewMDavis <mattmdavis@gmail.com>
2015-12-07 09:45:05 -0800

    ajax comment submission working

--- app/assets/javascripts/comments.coffee
+++ app/assets/javascripts/comments.coffee
@@ -1,3 +1,11 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on "page:change", ->
+  $("#comment-open").click (event) ->
+    event.preventDefault()
+    $("#comments-section").fadeToggle()
+    $("#comment_body").focus()
+
+
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,11 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+
+    respond_to do |format|
+      format.html { redirect_to article_path(@article) }
+      format.js #default handling, i.e. render comments/create.js.erb
+    end
   end
 
   def destroy
