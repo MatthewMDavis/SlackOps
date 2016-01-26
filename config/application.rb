c1fc2b4ca616119a353e91a099104644bf4f5c45
MatthewMDavis <mattmdavis@gmail.com>
2016-01-26 05:41:42 -0800

    switched to react_on_rails gem

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Rgblog
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.react.addons = true
     config.active_record.raise_in_transactional_callbacks = true
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
