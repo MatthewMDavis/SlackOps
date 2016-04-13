d459ae8080ec95333c781333d88d07bed9a62428
MatthewMDavis <mattmdavis@gmail.com>
2016-04-13 06:11:48 -0700

    implement conditional insertion of delete links, including cooling off

--- client/app/bundles/Comments/components/Comment.jsx
+++ client/app/bundles/Comments/components/Comment.jsx
@@ -1,37 +1,53 @@
 import React, { Component, PropTypes } from 'react';
 import marked from 'marked';
+import moment from 'moment';
 import { userMayDelete, commentIsFresh } from 'lib/components/policies';
 
 export default class Comment extends Component {
   static propTypes = {
     comment: PropTypes.object.isRequired,
-    user: PropTypes.object.isRequired,
+    user: PropTypes.object,
     articleAuthor: PropTypes.string.isRequired
   }
 
   constructor(props) {
     super(props);
+    this.displayDate = moment(props.comment.timestamp).format("MMM Do, Y HH:mm")
   }
 
   deleteLink(user, comment, articleAuthor) {
+
     if (userMayDelete(user, comment, articleAuthor)) {
-      return (<a href="#">Delete comment</a>);
+    // standard users have 5 minutes to delete their own comment (cooling
+    // off period); the article's author or site admins may delete at all times
+
+    const expires = moment(comment.timestamp).add(5, 'minutes').format('HH:mm');
+
+      switch (user.role) {
+        case 'user':
+          return <a href="#">Delete (until {expires})</a>;
+        default:
+          return <a href="#">Delete</a>;
+      }
     }
   }
 
   render() {
-  const { user, comment, articleAuthor } = this.props;
-  const rawMarkdown = marked(comment.body, { gfm: true, sanitize: true })
-  return (
-    <div id={`comment-${comment.id}`} className="comment">
-      <div className="comment-body">
-        <span dangerouslySetInnerHTML={{__html: rawMarkdown}} />
+    const { user, comment, articleAuthor } = this.props;
+    const rawMarkdown = marked(comment.body, { gfm: true, sanitize: true })
+
+    return (
+      <div id={`comment-${comment.id}`} className="comment">
+        <div className="comment-body">
+          <span dangerouslySetInnerHTML={{__html: rawMarkdown}} />
+        </div>
+        <div className="comment-byline">
+          By <a href={comment.commenter_url}>
+            {comment.commenter}
+          </a>&nbsp;
+              {this.displayDate} {this.deleteLink(user, comment, articleAuthor)}
+        </div>
       </div>
-      <div className="comment-byline">
-        By <a href={comment.commenter_url}>{comment.commenter}</a>
-        -- {comment.display_date} {this.deleteLink(user, comment, articleAuthor)}
-      </div>
-    </div>
-  );
+    );
   }
 }
