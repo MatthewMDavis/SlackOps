aaeddfc5768601c67941c581b691ac3652e3dc95
MatthewMDavis <mattmdavis@gmail.com>
2016-04-26 09:22:29 -0700

    and yet more

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" }, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks', :sessions => 'users/sessions', :registrations => 'users/registrations'}
 
+  get   '/login', :to => 'sessions#new', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => 'sessions#failure'
+
   root 'articles#index'
   resources :users
 
