2faaa8dcff492ac1325a596d4008944b810f8af5
MatthewMDavis <mattmdavis@gmail.com>
2016-05-03 07:59:39 -0700

    update gravatar for https

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -14,6 +14,6 @@ class UserSerializer < ActiveModel::Serializer
   # the email provided
   def gravatar_for(user, size)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
 end
