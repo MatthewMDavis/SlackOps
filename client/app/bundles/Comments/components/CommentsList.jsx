e8b712e0a3b503c8a1cbbcc5858192886ec86914
MatthewMDavis <mattmdavis@gmail.com>
2016-05-06 07:26:41 -0700

    revert CommentsList to functional component

--- client/app/bundles/Comments/components/CommentsList.jsx
+++ client/app/bundles/Comments/components/CommentsList.jsx
@@ -3,80 +3,29 @@ import Comment from '../components/Comment';
 import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup'
 import Immutable from 'immutable';
 
-export default class CommentsList extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  componentWillUpdate() {
-    this.xpos = window.scrollX;
-    this.ypos = window.scrollY;
-  }
-
-  componentDidUpdate() {
-    window.scrollTo(this.xpos, this.ypos);
-  }
-
-
-  render() {
-    const {
-      comments,
-      $$user,
-      article,
-      articleAuthor,
-      deleteCallback } = this.props;
-
-    const user = $$user ? $$user.toJS() : null;
-    const CSSTransitionGroup = require('react-addons-css-transition-group')
-    return (
-      <div id="comments-list">
-        <ReactCSSTransitionGroup
-          transitionName="comment"
-          transitionEnterTimeout={1000}
-          transitionLeaveTimeout={300} >
-          {comments.map(comment => {
-            return (
-              <Comment
-                key={comment.id}
-                comment={comment}
-                user={user}
-                article={article}
-                articleAuthor={articleAuthor}
-                deleteCallback={deleteCallback}
-              />
-            );
+export default ({ comments, $$user, article, articleAuthor, deleteCallback }) => {
+  const user = $$user ? $$user.toJS() : null;
+  const CSSTransitionGroup = require('react-addons-css-transition-group')
+  return (
+    <div id="comments-list">
+      <ReactCSSTransitionGroup
+        transitionName="comment"
+        transitionEnterTimeout={1000}
+        transitionLeaveTimeout={300} >
+        {comments.map(comment => {
+          return (
+                  <Comment
+                    key={comment.id}
+                    comment={comment}
+                    user={user}
+                    article={article}
+                    articleAuthor={articleAuthor}
+                    deleteCallback={deleteCallback}
+                  />
+                 );
           }
-        )}
-       </ReactCSSTransitionGroup>
-       <a name="list-end" />
-      </div>
-    );
-  }
+         )}
+      </ReactCSSTransitionGroup>
+    </div>
+  );
 }
-
-// export default ({ comments, $$user, article, articleAuthor, deleteCallback }) => {
-  // const user = $$user ? $$user.toJS() : null;
-  // const CSSTransitionGroup = require('react-addons-css-transition-group')
-  // return (
-    // <div id="comments-list">
-      // <ReactCSSTransitionGroup
-        // transitionName="comment"
-        // transitionEnterTimeout={1000}
-        // transitionLeaveTimeout={300} >
-        // {comments.map(comment => {
-          // return (
-                  // <Comment
-                    // key={comment.id}
-                    // comment={comment}
-                    // user={user}
-                    // article={article}
-                    // articleAuthor={articleAuthor}
-                    // deleteCallback={deleteCallback}
-                  // />
-                 // );
-          // }
-         // )}
-      // </ReactCSSTransitionGroup>
-    // </div>
-  // );
-// }
