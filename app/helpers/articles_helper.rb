20f7ed19fe99480ed1e235a87e5681df99a5131c
MatthewMDavis <mattmdavis@gmail.com>
2016-05-07 07:00:11 -0700

    fix markdown rendering on index page

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,7 +2,7 @@ module ArticlesHelper
   def excerpt(article)
     blurb = ""
     blurb << truncate(article.text, length: 1000, escape: false, separator: '. ')
-    sanitize(blurb)
+    markdown(blurb)
   end
 
 
