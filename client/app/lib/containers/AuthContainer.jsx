d23e4b5570be20b9b8a4d4d9d1e78120b24af893
MatthewMDavis <mattmdavis@gmail.com>
2016-05-15 11:38:04 -0700

    fix problem with facebook status not persisting between pages

--- client/app/lib/containers/AuthContainer.jsx
+++ client/app/lib/containers/AuthContainer.jsx
@@ -25,52 +25,6 @@ export default class AuthContainer extends Component {
     super(props, context);
   }
 
-
-  componentDidMount() {
-    const { $$authStore, dispatch } = this.props;
-    const curr_user = $$authStore.get('$$user');
-    const authActions = bindActionCreators(authActionCreators, dispatch);
-    const { logout } = authActions;
-
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId      : '1609870452669846',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v2.6',
-        status     : true
-      });
-      // -- Dev instance
-      // FB.init({
-        // appId      : '1611095332547358',
-        // cookie     : true,
-        // xfbml      : true,
-        // version    : 'v2.6',
-        // status    : true
-      // });
-
-      // If Facebook still has a live session, but there is no current user in
-      // redux state, we need to clean up.
-      FB.Event.subscribe('auth.authResponseChange', response => {
-        if (response.authResponse && !curr_user) {
-          console.log(response.authResponse);
-        }
-      })
-    }
-  }
-
-  componentWillMount() {
-    // Insert the Facebook JS sdk into the page headers
-    (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = '//connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  }
-
-
   render() {
     const { dispatch, $$authStore } = this.props;
     const authActions = bindActionCreators(authActionCreators, dispatch);
