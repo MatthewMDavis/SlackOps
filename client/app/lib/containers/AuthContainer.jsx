e8824f46ab47466efd761a8638babb4f831b40fe
MatthewMDavis <mattmdavis@gmail.com>
2016-05-15 07:38:36 -0700

    grrr

--- client/app/lib/containers/AuthContainer.jsx
+++ client/app/lib/containers/AuthContainer.jsx
@@ -51,12 +51,12 @@ export default class AuthContainer extends Component {
 
       // If Facebook still has a live session, but there is no current user in
       // redux state, we need to clean up.
+      FB.Event.subscribe('auth.authResponseChange', response => {
+        if (response.authResponse && !curr_user) {
+          logout();
+        }
+      })
     }
-    FB.Event.subscribe('auth.authResponseChange', response => {
-      if (response.authResponse && !curr_user) {
-        logout();
-      }
-    })
   }
 
   componentWillMount() {
